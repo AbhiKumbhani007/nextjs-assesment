eba48879fb85b72b6140f2c62e6423fa159ae584
abhishek <abhi1234kumbhani@gmail.com>
2023-08-06 19:18:02 +0530

    env added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,12 +6,11 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        "723641447928-2t96ci75c603mal831pfpnq9c51q22an.apps.googleusercontent.com", // Google client ID for authentication
-      clientSecret: "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", // Google client secret for authentication
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string, // Google client ID for authentication
+      clientSecret: process.env.NEXT_PUBLIC_GOOLE_CLIENT_SECRET as string, // Google client secret for authentication
     }),
   ],
-  secret: "9c2f6f50e05a0514b5e624f2d4e5a49b", // Secret used to encrypt and sign cookies and tokens
+  secret: process.env.NEXT_PUBLIC_SECRET as string, // Secret used to encrypt and sign cookies and tokens
 });
 
 // Export the NextAuth handler for both GET and POST requests
